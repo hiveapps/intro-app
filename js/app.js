f8ae7f4c58f368fb62f87b7116df6bfc4cc42a9d
atans2468 <austintans7@gmail.com>
2015-11-14 09:49:03 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Hive Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'helloWorld' is the name of this angular module example (also set in a <body> attribute in index.html)
+angular.module('helloWorld', [])
